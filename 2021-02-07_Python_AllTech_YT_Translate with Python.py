6c2f88701f9a2fe4a5222f03518a9bc452cb5364
erniejie <46618858+Erniejie@users.noreply.github.com>
2021-02-07 16:27:47 +0000

    Add files via upload

---
+++ 2021-02-07_Python_AllTech_YT_Translate with Python.py
@@ -0,0 +1,15 @@
+#Python Translate with Just 2 Lines of Code
+
+# AllTech_YT - Uploaded 7 Feb 2021
+
+# pip install translators
+
+import translators as ts
+
+text = "hola buenos dias"
+
+#print(ts.tencent(text,from_language="es",to_language="en"))
+#print(ts.bing(text,from_language="es",to_language="en"))
+#print(ts.baidu(text,from_language="es",to_language="en"))   # No Work
+#print(ts.sogou(text,from_language="es",to_language="en"))   # No Work
+print(ts.alibaba(text,from_language="es",to_language="en"))
